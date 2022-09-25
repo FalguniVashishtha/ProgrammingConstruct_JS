3f652e8143f4933e25256a2b2c7f352196e7a9b6
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-09-25 18:35:59 +0530

    prime range

---
+++ SelectionPracticeProblems/RepetitionPracticeProgrammes/PrimeInRange.js
@@ -0,0 +1,19 @@
+const readline = require("readline-sync");
+let lowerNumber = parseInt(readline.question("Enter the lower bound: "))
+let higherNumber = parseInt(readline.question("Enter the Upper bound: "))
+console.log(`The prime numbers between ${lowerNumber} and ${higherNumber} are:`);
+// looping from lowerNumber to higherNumber
+for (let i = lowerNumber; i <= higherNumber; i++) {
+    let count = 0;
+    // looping through 2 to user input number
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) {
+            count = 1;
+            break;
+        }
+    }
+    // if number greater than 1 and not divisible by other numbers
+    if (i > 1 && count == 0) {
+        console.log(i);
+    }
+}
