19c441c8a486317fd7c4df74221812eb56e277e1
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-09-25 18:26:49 +0530

    Harmonic number

---
+++ SelectionPracticeProblems/RepetitionPracticeProgrammes/HarmonicNumber.js
@@ -0,0 +1,10 @@
+const readline = require("readline-sync");
+let n = parseInt(readline.question("Enter the number(n): "))
+let harmonicNumber = 1;
+//we can use process.stdout.write() method to print to console without trailing newline.
+process.stdout.write("1");
+for(i=1; i<=n; i++){
+    process.stdout.write(" + 1/"+i);
+    harmonicNumber += 1/i;
+}
+console.log("\n"+n+" Harmonic Number is "+harmonicNumber);
